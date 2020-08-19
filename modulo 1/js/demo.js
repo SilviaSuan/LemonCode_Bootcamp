5b8d40657132e69ba180c82d2f78de9e606e36b2
Sílvia <silvia.suan@gmail.com>
2020-08-19 18:47:45 +0200

    práctica modulo 1

---
+++ modulo 1/js/demo.js
@@ -0,0 +1,3 @@
+document.getElementById("name").value = "Mi nombre";
+document.getElementById("lastName").value = "Mi apellido";
+document.getElementById("avatar").src = "img/undraw_female_avatar_w3jk.svg";
